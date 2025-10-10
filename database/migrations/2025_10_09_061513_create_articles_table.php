eafb7fb4e08b9d0518af4a5869a15bf9fb520482
Godzila121 <petadebchyk@gmail.com>
2025-10-10 17:54:40 +0300

    Мій перший коміт: додав початкові файли

---
+++ database/migrations/2025_10_09_061513_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+   public function up(): void
+   {
+       Schema::create('articles', function (Blueprint $table) {
+           $table->id();
+           $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+           $table->foreignId('topic_id')->nullable()->constrained()->nullOnDelete();
+           $table->string('title');
+           $table->string('slug')->unique();
+           $table->text('summary')->nullable();
+           $table->longText('content');
+           $table->string('cover_path')->nullable();
+           $table->boolean('is_premium')->default(false); // платний контент
+           $table->timestamps();
+       });
+   }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
